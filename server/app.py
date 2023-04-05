09b50583da1b06520679482df2cb1c9270673d3e
vargasmichael <119553981+vargasmichael@users.noreply.github.com>
2023-04-05 08:16:10 -0600

    lab solution

--- server/app.py
+++ server/app.py
@@ -19,5 +19,41 @@ db.init_app(app)
 def index():
     return "Index for Game/Review/User API"
 
+@app.route('/games')
+def games():
+    
+    games = []
+    for game in Game.query.all():
+        game_dict = {
+            "title": game.title,
+            "genre": game.genre,
+            "platform": game.platform,
+            "price": game.price,
+        }
+        games.append(game_dict)
+        
+    response = make_response(jsonify(games), 200,
+        {"Content-Type": "application/json"}                             
+)
+    
+    
+@app.route('/games/<int:id>')
+def game_by_id(id):
+    game = Game.query.filter(Game.id == id).first()
+    
+    game_dict = game.to_dict()
+    
+    # game_dict = {
+    #     "title": game.title,
+    #     "genre": game.genre,
+    #     "platform": game.platform,
+    #     "price": game.price,
+    # }
+    
+    response = make_response(game_dict, 200,)
+    response.headers["Content-Type"] = "application/json"
+
+    return response
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
